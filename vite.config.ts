0097a950ccd5c7846170a0b75af757ac0de56fa5
mayur bhoi <mayurbhoi200@gmail.com>
2025-06-12 11:17:57 +0000

    add react app optimie make it fast

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa';
+import viteCompression from 'vite-plugin-compression';
+import viteImagemin from 'vite-plugin-imagemin'
 
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    
+    viteImagemin({
+  gifsicle: { optimizationLevel: 7 },
+  optipng: { optimizationLevel: 7 },
+  mozjpeg: { quality: 70 },
+}),
+    viteCompression({ 
+   algorithm: 'brotliCompress', // or 'gzip'
+  ext: '.br',}),
     VitePWA({
   registerType: 'autoUpdate',
   includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
@@ -44,6 +53,14 @@ export default defineConfig({
   },
 })
   ],
+  optimizeDeps: {
+  include: ['react', 'react-dom', 'axios'], // Add your key deps
+},
+    build: {
+    target: 'esnext', // modern JS
+    minify: 'esbuild', // faster than terser
+    sourcemap: false, // disable unless needed
+  },
     server: {
     host: true,              // Allows external access (e.g. Gitpod)
     allowedHosts: true,     // Allows any domain name (Gitpod, etc.)
