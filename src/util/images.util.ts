ea7904de89ba9f4a9170e6132d09eec6d005e961
mayur bhoi <mayurbhoi200@gmail.com>
2025-06-12 09:42:51 +0530

    resolve bioeader isuue

--- src/util/images.util.ts
+++ src/util/images.util.ts
@@ -1,6 +1,6 @@
 import welcome from "../assets/images/login.png"
 import bioHeader from "../assets/images/bioHeader.png"
-import bioHeader1 from "../assets/images/bioHeader1.png"
+import bioHeader1 from "../assets/images/bioheader1.png"
 import searchbanner from "../assets/images/searchBanner.png"
 
 
