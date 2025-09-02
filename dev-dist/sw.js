f0412465e6f28ae7d7b51b1562b6ca36424e610f
mayur lotan bbhoi <mayurbhoi200@gmail.com>
2025-09-02 10:59:57 +0530

    update sw.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.52al8o94nto"
+    "revision": "0.qdf2ur04o3g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
